f5ea837fbe84866057a09b6a47e975ad0a64ad59
Solomon Hilliard <shillia6@eagles.nccu.edu>
2015-02-19 03:03:51 -0500

    First four tests done

--- test/models/author_test.rb
+++ test/models/author_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class AuthorTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  setup do
+  end
+
+  test "data exists" do
+    assert_equal "Teddy R", authors(:one).name
+  end
+
+  test "unique works" do
+    teddy2 = Author.new(name: "Teddy S", email: "rose@red.com", password_digest: "goat")
+    refute teddy2.save
+  end
+
+  test "database saves new changes" do
+    Author.create(name: "Taft Possem", email: "taft@man.com", password_digest: "goat")
+    assert_equal 3, Author.count
   end
 end
