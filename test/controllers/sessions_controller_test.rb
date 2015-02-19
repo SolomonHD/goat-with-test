8bad0eb6cb9a2358869f829c57dd8ada23004d7f
Solomon Hilliard <shillia6@eagles.nccu.edu>
2015-02-19 10:05:11 -0500

    More tests added

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,5 +1,13 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
+  test "show sessions page" do
+    get :log_in
+    assert_response :success
+  end
+
+  test "show login fields" do
+    assert_select("login-email")
+  end
 
 end
